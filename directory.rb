3d6de860af6efd4672718b2f015f519d7a3b2ea9
Ben230 <benarthurstuart@hotmail.com>
2019-10-11 16:27:27 +0000

    add 3rd save option

--- directory.rb
+++ directory.rb
@@ -26,6 +26,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -35,6 +37,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to student.csv"
   puts "9. Exit" # 9 because we'll be adding more items  
 end
 def print_header
@@ -53,6 +56,17 @@ def show_students
   print_header
   print_students_list
   print_footer
+end
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 #nothing happens until we call the methods
 interactive_menu
